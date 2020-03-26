6ac851d91098b76506c95f6d959f5141a2cd6c3d
igorwessel <igor_wessel@hotmail.com>
2020-03-26 19:49:50 -0300

    feat: Finish backend :rocket:

--- backend/src/controllers/IncidentController.js
+++ backend/src/controllers/IncidentController.js
@@ -2,9 +2,30 @@ const connection = require('../database/connection')
 
 module.exports = {
     async index(req, res){
-        const incidents = await connection('incidents').select('*')
+        const { page = 1 } = req.query
+
+        const [count] = await connection('incidents').count();
+
+        console.log(count)
+
+        const incidents = await connection('incidents')
+                            .join('ongs', 'ongs.id', '=', 'incidents.ong_id')
+                            .limit(5)
+                            .offset((page - 1) * 5)
+                            .select([
+                                'incidents.*', 
+                                'ongs.name', 
+                                'ongs.email', 
+                                'ongs.whatsapp',
+                                'ongs.city',
+                                'ongs.uf'
+                            ]);
+
+        res.header('X-Total-Count', count['count(*)'])
+
         return res.json(incidents)
     },
+
     async store(req, res){
         const { title, description, value } = req.body
         const ong_id = req.headers.authorization
@@ -17,5 +38,23 @@ module.exports = {
         })
 
         return res.json({ id })
-    }
+    },
+    
+    async delete(req, res) {
+        const { id } = req.params
+        const ong_id = req.headers.authorization
+        
+        const incident = await connection('incidents')
+            .where('id', id)
+            .select('ong_id')
+            .first();
+        
+        if (incident.ong_id !== ong_id)
+            return res.status(401).json({ error: 'Operation not permited. '});
+
+        await connection('incidents').where('id', id).delete()
+
+        return res.status(204).send();
+        
+    }   
 }
--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,15 +1,21 @@
 const express = require('express')
 const OngController = require('./controllers/OngController')
-
 const IncidentController = require('./controllers/IncidentController')
+const ProfileController = require('./controllers/ProfileController')
+const SessionController = require('./controllers/SessionController')
 
-const connection = require('./database/connection')
 
 const routes = express.Router()
+
+routes.post('/sessions', SessionController.create)
 
 routes.post('/ongs', OngController.store)
 routes.get('/ongs', OngController.index)
 
+routes.get('/profile', ProfileController.index)
+
 routes.post('/incidents', IncidentController.store)
 routes.get('/incidents', IncidentController.index)
+routes.delete('/incidents/:id', IncidentController.delete)
+
 module.exports = routes
