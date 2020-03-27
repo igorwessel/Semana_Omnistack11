a998d3dd8b617511cafb7a0738a284e5816301f1
igorwessel <igor_wessel@hotmail.com>
2020-03-26 21:00:17 -0300

    feat: Create page Register. :rocket:

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,12 @@
 import React from 'react';
-import Logon from './pages/Logon'
+import Routes from './routes'
 import './global.css'
 
 const App = ()  => {
 
   return (
     <div>
-      <Logon />
+      <Routes />
     </div>
   );
 }
